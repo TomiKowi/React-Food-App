b10bf65f222ce94eb80920598ba06e43e64d1f20
Tomek Kowol <tomek.pdp@gmail.com>
2019-06-27 18:31:01 +0200

    create folder "pages" with new components

---
+++ my-app/src/pages/DrinkPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DrinkPage = () => {
+    return (
+        <h2>DrinkPage</h2>
+    );
+}
+
+export default DrinkPage;
---
+++ my-app/src/pages/FoodPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FoodPage = () => {
+    return (
+        <h2>FoodPage</h2>
+    );
+}
+
+export default FoodPage;
