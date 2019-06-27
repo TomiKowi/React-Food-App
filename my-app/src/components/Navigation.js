3986c27f0512db06228dd9dda09e1deeff1beb71
Tomek Kowol <tomek.pdp@gmail.com>
2019-06-27 18:32:25 +0200

    add path to pages components

--- my-app/src/components/Navigation.js
+++ my-app/src/components/Navigation.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import '../styles/Navigation.css';
 
+const navList = [
+    { name: "Home", path: "/", exact: true },
+    { name: "Food", path: "/Food" },
+    { name: "Drink", path: "/Drink" },
+    { name: "Favorites", path: "/Favorites" },
+]
+
 const Navigation = () => {
+
+
+    const list = navList.map(item => (
+        <li key={item.name}><NavLink to={item.path} exact={item.exact ? item.exact : false}>{item.name}</NavLink></li>
+    ))
     return (
         <nav className="navigation">
-            <h2>Navigation</h2>
+            <ul>
+                {list}
+            </ul>
         </nav>
     );
 }
--- my-app/src/components/Result.js
+++ my-app/src/components/Result.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from '../pages/HomePage';
+import FoodPage from '../pages/FoodPage';
+import DrinkPage from '../pages/DrinkPage';
+import FavoritesPage from '../pages/FavoritesPage';
 import '../styles/Result.css';
 
 const Result = () => {
     return (
-        <div className="result">
-            <h2>Result</h2>
-        </div>
+        <>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/Food" exact component={FoodPage} />
+            <Route path="/Drink" exact component={DrinkPage} />
+            <Route path="/Favorites" exact component={FavoritesPage} />
+        </>
     );
 }
 
