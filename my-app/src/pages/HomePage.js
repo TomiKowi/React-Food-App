3898c005a527acfb429287d476088613efe47b96
Tomek Kowol <tomek.pdp@gmail.com>
2019-07-02 22:16:16 +0200

    add some lorem ipsum to paragraph

--- my-app/src/pages/HomePage.js
+++ my-app/src/pages/HomePage.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import '../styles/HomePage.css'
 
 const HomePage = () => {
     return (
-        <h2>HomePage</h2>
+        <div className="introduction">
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium repudiandae corporis adipisci doloremque iusto velit atque eos tenetur. Placeat saepe tempore distinctio laboriosam facere nobis obcaecati accusamus, molestiae magnam debitis!
+                Eaque quo incidunt molestias dolores culpa adipisci cumque odio voluptatem nesciunt. Mollitia eligendi dolorem ea fugit quas incidunt voluptate sequi explicabo dolore minus, laudantium fuga vitae neque rem, molestiae commodi.
+                Fugiat facilis quasi laboriosam nemo excepturi exercitationem quaerat voluptate dolorem? Esse aperiam, ea corrupti, impedit commodi sed dignissimos atque, ab ipsam earum non veritatis explicabo sint dolorum deleniti modi nihil.
+                Aliquam adipisci exercitationem omnis ipsum itaque assumenda suscipit excepturi, ducimus quibusdam sit quo deserunt consequatur minus ratione dolores cumque debitis odio quidem ex harum! Maiores mollitia sapiente quas neque amet.
+                A, temporibus assumenda ex excepturi repudiandae dolor veniam placeat modi accusantium nam in. Pariatur, veritatis minima, ipsum dolorum illum tempora quisquam et consectetur est ratione assumenda sed aspernatur voluptatum architecto.
+        </p>
+        </div>
     );
 }
 
