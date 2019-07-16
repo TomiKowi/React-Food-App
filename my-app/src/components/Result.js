de72efde64c5023775dd3f9ddcfdab64f9240942
Tomek Kowol <tomek.pdp@gmail.com>
2019-07-16 21:16:00 +0200

    remove Favorites

--- my-app/src/components/Result.js
+++ my-app/src/components/Result.js
@@ -3,7 +3,6 @@ import { Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import FoodPage from '../pages/FoodPage';
 import DrinkPage from '../pages/DrinkPage';
-import FavoritesPage from '../pages/FavoritesPage';
 import '../styles/Result.css';
 
 const Result = () => {
@@ -13,7 +12,6 @@ const Result = () => {
             <Route path="/" exact component={HomePage} />
             <Route path="/Food" exact component={FoodPage} />
             <Route path="/Drink" exact component={DrinkPage} />
-            <Route path="/Favorites" exact component={FavoritesPage} />
         </>
     );
 }
